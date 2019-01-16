784ce6cc0f3c9f9001c3b6eefb7c93274bf7cad3
Duongninh0110 <duongninh0110@gmail.com>
2019-01-16 10:28:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import {Loading} from './Loading';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    //state
+    this.state = {
+      users: [],
+      loading: false
+    }
+    //bind handleSubmit to App class
+    this.hanleSubmit = this.hanleSubmit.bind(this);
+  }
+
+  getUsers() {
+    this.setState({
+          loading: true
+        });
+
+    axios('https://api.randomuser.me/?nat=US&results=5').then(response => this.setState({
+      users: [...this.state.users,...response.data.results],
+      loading: false
+    }));
+  }
+
+  hanleSubmit(e) {
+    e.preventDefault();
+    this.getUsers();
+    console.log('more users loaded');
+  }
+
+
+  componentWillMount() {
+    this.getUsers();
+  }
   render() {
+    const {users, loading} = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <form onSubmit={this.hanleSubmit}>
+          <input type="submit" value="load users"/>
+        </form>
+        <hr/>
+
+        {!loading ? users.map(user => 
+          (<div key = {user.id.value}>
+              <h3 style = {{color: 'red'}}>{user.name.first}</h3>
+              <p>{user.email}</p>
+              <hr/>
+              
+            </div>))
+          : (<Loading message = "DMM"/>)}
       </div>
-    );
+      )
+    }
   }
-}
 
 export default App;
